872dee9bbe954c4127b4980f9873aa30fa7522ed
CotinMihai <cotinmihai@gmail.com>
2026-04-18 18:28:42 +0300

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create a web server from scratch
+const http = require('http');
+
+
+
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello, World!\n');
+});
+
+
+
